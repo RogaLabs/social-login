eaf10ff96cf7cc37e3c3acbe00999968bb227fa0
oliveiradev <felipe.oliveiradev@gmail.com>
2016-07-19 19:30:59 -0300

    add token field to social user

--- lib/src/main/java/com/rogalabs/lib/SocialUser.kt
+++ lib/src/main/java/com/rogalabs/lib/SocialUser.kt
@@ -1,9 +1,8 @@
 package com.rogalabs.lib;
 
-import android.net.Uri
 import java.io.Serializable
 
 /**
  * Created by roga on 08/07/16.
  */
-data class SocialUser(val id: String?, val name: String?, val email: String?, val photoUrl: Uri? = null) : Serializable
+data class SocialUser(val id: String? , val name: String?, val email: String?, val photoUrl: String? = "", val token: String? = "") : Serializable
